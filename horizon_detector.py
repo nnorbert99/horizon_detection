6adc676047dfe660222616cbd93938484e7c6ae5
carla-project <gamerhuer@gmail.com>
2022-03-09 19:01:35 +0100

    Adding canny edge detector and hough transform top five most voted line selected

--- horizon_detector.py
+++ horizon_detector.py
@@ -1,1 +1,12 @@
+import cv2 as cv
+import os
+import image_preprocessor as im
+import numpy as np
 
+cwd = os.getcwd()
+path = os.path.join(cwd, 'Source Images')
+
+image = cv.imread(os.path.join(path, 'example.jpg'))
+processed_image = im.preprocess(image)
+edge_image = cv.Canny(processed_image, 200, 255, L2gradient=True)
+lines = cv.HoughLines(edge_image, 1, np.pi / 180, 0)
