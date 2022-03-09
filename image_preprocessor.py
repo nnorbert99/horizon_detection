749c6b086449f0ea2ca43815bf0c3391f7f68c7e
carla-project <gamerhuer@gmail.com>
2022-03-09 19:00:06 +0100

    Adding fcn to visualize canny parameters

--- image_preprocessor.py
+++ image_preprocessor.py
@@ -29,3 +29,33 @@ def blur_image(img: numpy.ndarray, mode: str) -> numpy.ndarray:
     else:
         print('Invalid mode')
         return None
+
+def visualise_canny_thresholds(img: numpy.ndarray):
+    """
+
+    :param img:
+    :return:
+    """
+    min_val = 0
+    max_val = 255
+    font = cv.FONT_HERSHEY_SIMPLEX
+
+    while True:
+        edge_image = cv.Canny(img, min_val, max_val, L2gradient=True)
+        cv.putText(edge_image, 'min' + str(min_val) + '   max' + str(max_val), (30, 30), font, 1, (255, 255, 255), 2,
+                   cv.LINE_AA)
+        cv.imshow('Display', edge_image)
+        k = cv.waitKey(0)
+        if k == ord('o'):
+            min_val += 10
+        elif k == ord('O'):
+            min_val -= 10
+        elif k == ord('l'):
+            max_val -= 10
+        elif k == ord('L'):
+            max_val += 10
+        elif k == ord('j'):
+            break
+        else:
+            print(' o --> increase min threshold \n O --> decrease \n l --> '
+                  'decrease max threshold \n L --> increase \n j --> exit')
