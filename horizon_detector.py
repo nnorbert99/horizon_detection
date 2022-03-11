2b889ade7556720677cb01e50c6d6ce3a48284e3
carla-project <gamerhuer@gmail.com>
2022-03-11 17:08:05 +0100

    More advanced file management

--- horizon_detector.py
+++ horizon_detector.py
@@ -6,6 +6,11 @@ import numpy as np
 cwd = os.getcwd()
 path = os.path.join(cwd, 'Source Images')
 
+if not os.path.exists(path):
+    os.makedirs(path)
+    print(f'No Source Image directory found we created one for you at {path}')
+
+file_paths = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 image = cv.imread(os.path.join(path, 'example.jpg'))
 processed_image = im.preprocess(image)
 edge_image = cv.Canny(processed_image, 200, 255, L2gradient=True)
