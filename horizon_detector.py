39323a9e7cf4be0e85026aed63e5c176b00ef20d
carla-project <gamerhuer@gmail.com>
2022-03-11 17:21:34 +0100

    Refactor code

--- horizon_detector.py
+++ horizon_detector.py
@@ -14,13 +14,12 @@ if not os.path.exists(path):
 
 file_paths = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 picture_paths = [f for f in file_paths if imghdr.what(f) in ['jpg', 'png', 'bmp', 'gif', 'tiff', 'jpeg']]
-image = cv.imread(os.path.join(path, 'example.jpg'))
-processed_image = im.preprocess(image)
-edge_image = cv.Canny(processed_image, 200, 255, L2gradient=True)
-lines = cv.HoughLines(edge_image, 1, np.pi / 180, 0)
-processed_image = im.draw_hough_lines(processed_image, lines)
-images = [image, processed_image, edge_image]
 
-for img in images:
-    cv.imshow('Display', img)
+for pic_path in picture_paths:
+    image = cv.imread(pic_path)
+    processed_image = im.preprocess(image)
+    edge_image = cv.Canny(processed_image, 200, 255, L2gradient=True)
+    lines = cv.HoughLines(edge_image, 1, np.pi / 180, 0)
+    processed_image = im.draw_hough_lines(processed_image, lines,1)
+    cv.imshow('Display', processed_image)
     cv.waitKey(0)
